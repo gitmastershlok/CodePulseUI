141eaf46e34de4eef09a0a97676d421c0c4fc1e5
Shlok B. Gupta <shlok.gupta_cs19@gla.ac.in>
2023-09-04 18:02:38 +0530

    First project commit & push.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './core/components/navbar/navbar.component';
+import { CategoryListComponent } from './features/category/category-list/category-list.component';
+import { AddCategoryComponent } from './features/category/add-category/add-category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CategoryListComponent,
+    AddCategoryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
